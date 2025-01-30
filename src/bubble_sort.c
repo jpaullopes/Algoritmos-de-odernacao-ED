98624520cd1d3b9d073fa5216db195a9137d1c90
JotaPe_Lopes <jpaullopes27@gmail.com>
2025-01-30 12:02:38 -0300

    edit: bubble_sort.c

--- src/bubble_sort.c
+++ src/bubble_sort.c
@@ -1,14 +1,22 @@
 #include "bubble_sort.h"
 
 void bubble_sort(int *array, int tamanho) {
-    for (int i = 0; i < tamanho - 1; i++) { // Percorre o array
-        for (int j = 0; j < tamanho - i - 1; j++) { // Percorre o array - i - 1
-            if (array[j] > array[j + 1]) { // Se o elemento atual for maior que o próximo
+    int i, j;
+    int trocado;
+    for (i = 0; i < tamanho - 1; i++) { // Percorre o array
+        trocado = 0; // Inicializa a variável de verificação de troca
+        for (j = 0; j < tamanho - i - 1; j++) { // Percorre o array - 1
+            if (array[j] > array[j + 1]) { // Verifica se o elemento atual é maior que o próximo
                 // Troca os elementos
-                int temp = array[j]; // Variável temporária
-                array[j] = array[j + 1]; // Troca
-                array[j + 1] = temp; // Troca
+                int temp = array[j];
+                array[j] = array[j + 1];
+                array[j + 1] = temp;
+                trocado = 1; // Marca que houve uma troca
             }
+        }
+        // Se não houve trocas, o array já está ordenado
+        if (trocado == 0) {
+            break;
         }
     }
 }
